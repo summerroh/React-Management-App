d0dfcf9147ba67ba65535bf7a0c19cfd7d6954ae
summerroh <summerroh@github.com>
2020-04-27 22:57:21 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+var customers=[
+  {
+  'id': 1,
+  'image': 'http://placeimg.com/64/64/any/1',
+  'name':"노선경",
+  'birthday':"950211",
+  'gender':"여자",
+  'job':"디자이너"
+},
+{
+  'id': 2,
+  'image': 'http://placeimg.com/64/64/any/2',
+  'name':"마틴시",
+  'birthday':"920101",
+  'gender':"남자",
+  'job':"음악가"
+},
+{
+  'id': 3,
+  'image': 'http://placeimg.com/64/64/any/3',
+  'name':"무무얌",
+  'birthday':"970512",
+  'gender':"여자",
+  'job':"개발자"
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's Develop Management System!</h2>
-    </div>
-  );
-}
+    <div>{
+      customers.map(c => {
+        return (
+          <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+          )
+        })
+      }
+   </div>
+  )}
+        
 
 export default App;
