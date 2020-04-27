c17e1bfdd1d19393fdded642950ce29f1113cc81
summerroh <summerroh@github.com>
2020-04-27 21:39:02 +0900

    add everything

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"></img>
+      <h2>Let's Develop Management System!</h2>
+    </div>
+  );
+}
+
+export default App;
